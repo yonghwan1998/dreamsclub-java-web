a1726e412ff9ab46b5f783c9a1e60ae63b3a05dd
user <user@BOOK-LCCUUB9T5H>
2023-09-05 20:47:20 +0900

    qna 와이라노

---
+++ dreams/src/main/java/xyz/dreams/mapper/QnaMapper.java
@@ -0,0 +1,29 @@
+package xyz.dreams.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import xyz.dreams.dto.QnaDTO;
+
+public interface QnaMapper {
+	/* QnA  등록*/
+	int enrollQna(QnaDTO qna);
+	
+	/* QnA 수정*/
+	int modifyQna(QnaDTO qna);
+	
+	/* QnA 삭제*/
+	int deleteQna(int qnaNo);
+	
+
+	
+	/* QnA 페이징 처리*/
+	List<QnaDTO> selectQnaList(Map<String, Object> map);
+	int selectQnaCount();
+	
+	
+	
+	
+	
+
+}
