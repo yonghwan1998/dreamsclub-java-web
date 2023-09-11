b06305f3eb1324164fe120892e6539ced7c5c00d
yonghwan1998 <bbl737898@gmail.com>
2023-09-11 12:30:02 +0900

    메인 페이지 굿즈 출력 기능

--- dreams/src/main/java/xyz/dreams/controller/HomeController.java
+++ dreams/src/main/java/xyz/dreams/controller/HomeController.java
@@ -1,13 +1,36 @@
 package xyz.dreams.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.RequiredArgsConstructor;
+import xyz.dreams.dto.GoodsDTO;
+import xyz.dreams.service.GoodsService;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+	private final GoodsService goodsService; 
 
+	/*
+	방용환(수정) : 2023/09/11, 메인 페이지 굿즈 리스트 출력
+	리뷰 or 별점순으로 출력 예정
+	*/
 	@RequestMapping("/")
-	public String home() {
+	public String home(@RequestParam(defaultValue = "goods_code") String column, Model model) {
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("column", column);
+		
+		List<GoodsDTO> goodsList = goodsService.getGoodsList(map);
+		model.addAttribute("goodsList", goodsList);
+		
 		return "mainpage/index";
 	}
 
