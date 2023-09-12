f5bc262a773831af06354df4b11e1c5f1a163724
yonghwan1998 <bbl737898@gmail.com>
2023-09-12 18:22:01 +0900

    방용환 : 23.09.12, 굿즈 메인 페이지 카테고리 선택 검색 기능 추가 (3개 카테고리 전부 제거 시 예외 발생 수정 필요)

--- dreams/src/main/java/xyz/dreams/controller/HomeController.java
+++ dreams/src/main/java/xyz/dreams/controller/HomeController.java
@@ -19,21 +19,34 @@ public class HomeController {
 	private final GoodsService goodsService; 
 
 	/*
-	방용환(수정) : 2023/09/11, 메인 페이지 굿즈 리스트 출력
-	- 리뷰 or 별점순으로 출력 예정
-	방용환(수정) : 2023/09/12, 메인 페이지 오류 수정
-	- minPrice, maxPrice 미설정으로 인한 IndexOutOfBounds 오류 수정
+	- 방용환(수정) : 2023/09/11, 메인 페이지 굿즈 리스트 출력
+	리뷰 or 별점순으로 출력 예정
+	- 방용환(수정) : 2023/09/12, 메인 페이지 오류 수정
+	minPrice, maxPrice 미설정으로 인한 IndexOutOfBounds 오류 수정
+	- 방용환(수정) : 2023/09/12, 메인 페이지 오류 수정
+	uniform, cap, fan 미설정으로 인한 IndexOutOfBounds 오류 수정
 	*/
 	@RequestMapping("/")
 	public String home(@RequestParam(defaultValue = "goods_code") String column,
 			@RequestParam(defaultValue = "9999999") int maxPrice, @RequestParam(defaultValue = "0") String minPrice,
-			Model model) {
+			@RequestParam(defaultValue = "true") Boolean uniform, @RequestParam(defaultValue = "true") Boolean cap,
+			@RequestParam(defaultValue = "true") Boolean fan, Model model) {
 
+		// column:정렬순서, minPrice:최소 금액. maxPrice:최대 금액,
+		// uniform:유니폼 카테고리, cap:모자 카테고리, fan:팬 상품 카테고리 등의 값을
+		// goodsService에 인자로 넘기기 위한 Map
 		Map<String, Object> map = new HashMap<>();
+
 		map.put("column", column);
+
 		map.put("maxPrice", maxPrice);
 		map.put("minPrice", minPrice);
 
+		map.put("uniform", uniform);
+		map.put("cap", cap);
+		map.put("fan", fan);
+
+		// 검색 조건들을 goodsService.getGoodsList(Map<String, Object> map)에 인자로 넘김
 		List<GoodsDTO> goodsList = goodsService.getGoodsList(map);
 		model.addAttribute("goodsList", goodsList);
 
