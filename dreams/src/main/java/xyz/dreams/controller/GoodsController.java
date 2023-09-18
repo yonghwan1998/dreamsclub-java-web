6d600b0100c49de77175000b82ba78285017c0a5
Jellogg2 <Jellogg2@DESKTOP-CQH6GPQ>
2023-09-18 09:57:22 +0900

    goodscontroller - 오진서

--- dreams/src/main/java/xyz/dreams/controller/GoodsController.java
+++ dreams/src/main/java/xyz/dreams/controller/GoodsController.java
@@ -9,18 +9,13 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.RequiredArgsConstructor;
-import xyz.dreams.dto.CommunityReplyDTO;
 import xyz.dreams.dto.GoodsDTO;
 import xyz.dreams.dto.QnaDTO;
 import xyz.dreams.service.GoodsService;
